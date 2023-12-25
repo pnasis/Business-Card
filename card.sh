7b454659807fe6d146a8aa89dc54de14b4070822
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2023-12-25 21:43:42 +0200

    Add files via upload

---
+++ card.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+###############################################
+# Write a nice business card on the terminal. #
+###############################################
+
+# Bitly = https://raw.githubusercontent.com/tallguyjenks/BusinessCard/master/business_card
+
+# ╭───────────────────────────────────────────────────────╮
+# │                                                       │
+# │           Michael Tsouchlarakis / michaeltd           │
+# │                                                       │
+# │      Work:  tsouchlarakis@gmail.com                   │
+# │      FOSS:  Gentoo Linux avocado.                     │
+# │                                                       │
+# │   Twitter:  https://twitter.com/tsouchlarakismd       │
+# │       npm:  https://npmjs.com/~michaeltd              │
+# │    GitHub:  https://github.com/michaeltd              │
+# │  LinkedIn:  https://linkedin.com/in/michaeltd         │
+# │       Web:  https://michaeltd.netlify.com/            │
+# │                                                       │
+# │      Card:  curl -sL https://tinyurl.com/mtd-card|sh  │
+# │                                                       │
+# ╰───────────────────────────────────────────────────────╯
+
+######################################
+# Font attributes, colors, bg colors #
+######################################
+#bg_black="$(tput setab 0)"
+#bg_blue="$(tput setab 4)"
+#bg_cyan="$(tput setab 6)"
+#bg_default="$(tput setab 9)"
+#bg_green="$(tput setab 2)"
+#bg_magenta="$(tput setab 5)"
+#bg_red="$(tput setab 1)"
+#bg_white="$(tput setab 7)"
+#bg_yellow="$(tput setab 3)"
+#black="$(tput setaf 0)"
+#blink="$(tput blink)"
+#default="$(tput setaf 9)"
+#end_underline="$(tput rmul)"
+#hidden="$(tput invis)"
+#reverse="$(tput rev)"
+#white="$(tput setaf 7)"
+blue="$(tput setaf 4)"
+bold="$(tput bold)"
+cyan="$(tput setaf 6)"
+dim="$(tput dim)"
+green="$(tput setaf 2)"
+magenta="$(tput setaf 5)"
+red="$(tput setaf 1)"
+reset="$(tput sgr0)"
+underline="$(tput smul)"
+yellow="$(tput setaf 3)"
+
+cat <<EOF
+
+${cyan}╭───────────────────────────────────────────────────────╮
+${cyan}│                                                       │
+${cyan}│${reset}           ${bold}${cyan}Prodromos Nasis / pnasis${reset}                  ${cyan}│
+${cyan}│                                                       │
+${cyan}│${reset}     ${underline}${bold}Email:${reset}  ${magenta}prodromos.nasis@gmail.com                 ${cyan}│
+${cyan}│${reset}       ${underline}${bold}Web:${reset}  ${dim}https://www.${reset}${cyan}pnasis.github.io                ${cyan}|
+${cyan}│                                                       │
+${cyan}│${reset}    ${underline}${bold}GitHub:${reset}  ${dim}https://github.com/${reset}${green}pnasis           ${cyan}│
+${cyan}│${reset}  ${underline}${bold}LinkedIn:${reset}  ${dim}https://linkedin.com/in/${reset}${blue}prodromos-nasis        ${cyan}│${cyan}│                                                       │
+${cyan}│${reset}      ${underline}${bold}Card:${reset}  ${yellow}curl -sL bit.ly/2zvxUbd | sh              ${cyan}│
+${cyan}│                                                       │
+${cyan}╰───────────────────────────────────────────────────────╯
+
+EOF
